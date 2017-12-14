ad90b2fc01cc15f0e29e90448660cc648327a9b7
davgutavi <davgutavi@gmail.com>
2017-12-14 19:30:22 +0100

    Commit 20

--- api/carga_paquetes_variables_globales.R
+++ api/carga_paquetes_variables_globales.R
@@ -3,6 +3,7 @@ require(ggplot2)
 require(dplyr)
 require(reshape2)
 require(ggrepel)
+require(RColorBrewer)
 
 os <- "MACOSX"
 #os <- "LINUX"
@@ -11,12 +12,10 @@ os <- "MACOSX"
 if (os == "MACOSX"){
   
   ##Carga SparkR en Mac OSX
-  
-  #spark_path <- strsplit(system("brew info apache-spark",intern=T)[4],' ')[[1]][1] # Get your spark path
-  spark_path <- "/usr/local/Cellar/apache-spark/2.2.0"
-  .libPaths(c(file.path(spark_path,"libexec", "R", "lib"), .libPaths())) # Navigate to SparkR folder
-  require(SparkR) # Load the library
-  sparkR.session(master = "local[*]")
+  Sys.setenv(SPARK_HOME = "/Users/davgutavi/Spark/spark-2.2.1-bin-hadoop2.7")
+  Sys.setenv(JAVA_HOME = "/Library/Java/JavaVirtualMachines/jdk1.8.0_152.jdk/Contents/Home")
+  library(SparkR, lib.loc = c(file.path(Sys.getenv("SPARK_HOME"), "R", "lib")))
+  sparkR.session(master = "local[*]", sparkConfig = list(spark.driver.memory = "2g"))
   
 }else{
   
@@ -32,26 +31,26 @@ if (os == "MACOSX"){
 rootHDFSdatasets <- "hdfs://192.168.47.247/user/datos/endesa/datasets/"
 rrootHDDavid <-"/Volumes/david/"
 rootHDDLab <- "/mnt/datos/"
-rootLocalDatasets <-"/Users/davgutavi/Desktop/endesa/datasets/"
+rootLocalDatasets <-"/Users/davgutavi/NAS_DAVGUTAVI/endesa/investigacion_paper/datasets/"
 rootLocalClasificacion <-"/Users/davgutavi/Desktop/endesa/clasificacion/"
-rootLocalClustering <-"/Users/davgutavi/Desktop/endesa/clustering/"
+rootLocalClustering <-"/Users/davgutavi/NAS_DAVGUTAVI/endesa/investigacion_paper/clustering/"
 
 
 #*******************************************CLUSTERING EXPERIMETS
 
-pathL_454d_raw_umr_kmeans<-paste0(rootLocalClustering,"little_454d_raw_umr_kmeans")
-pathL_454d_raw_con_kmeans<-paste0(rootLocalClustering,"little_454d_raw_con_kmeans")
-pathL_454d_max_umr_kmeans<-paste0(rootLocalClustering,"little_454d_max_umr_kmeans")
-pathL_454d_max_con_kmeans<-paste0(rootLocalClustering,"little_454d_max_con_kmeans")
-pathL_454d_slo_umr_kmeans<-paste0(rootLocalClustering,"little_454d_slo_umr_kmeans")
-pathL_454d_slo_con_kmeans<-paste0(rootLocalClustering,"little_454d_slo_con_kmeans")
+l_path_454d_raw_umr_kmeans<-paste0(rootLocalClustering,"lc_454d_raw_umr")
+l_path_454d_raw_con_kmeans<-paste0(rootLocalClustering,"lc_454d_raw_con")
+l_path_454d_max_umr_kmeans<-paste0(rootLocalClustering,"lc_454d_max_umr")
+l_path_454d_max_con_kmeans<-paste0(rootLocalClustering,"lc_454d_max_con")
+l_path_454d_slo_umr_kmeans<-paste0(rootLocalClustering,"lc_454d_slo_umr")
+l_path_454d_slo_con_kmeans<-paste0(rootLocalClustering,"lc_454d_slo_con")
 
-pathL_364d_slo_umr_kmeans<-paste0(rootLocalClustering,"little_364d_raw_umr_kmeans")
-pathL_364d_slo_con_kmeans<-paste0(rootLocalClustering,"little_364d_raw_con_kmeans")
-pathL_364d_raw_umr_kmeans<-paste0(rootLocalClustering,"little_364d_max_umr_kmeans")
-pathL_364d_raw_con_kmeans<-paste0(rootLocalClustering,"little_364d_max_con_kmeans")
-pathL_364d_max_umr_kmeans<-paste0(rootLocalClustering,"little_364d_slo_umr_kmeans")
-pathL_364d_max_con_kmeans<-paste0(rootLocalClustering,"little_364d_slo_con_kmeans")
+l_path_364d_slo_umr_kmeans<-paste0(rootLocalClustering,"lc_364d_raw_umr")
+l_path_364d_slo_con_kmeans<-paste0(rootLocalClustering,"lc_364d_raw_con")
+l_path_364d_raw_umr_kmeans<-paste0(rootLocalClustering,"lc_364d_max_umr")
+l_path_364d_raw_con_kmeans<-paste0(rootLocalClustering,"lc_364d_max_con")
+l_path_364d_max_umr_kmeans<-paste0(rootLocalClustering,"lc_364d_slo_umr")
+l_path_364d_max_con_kmeans<-paste0(rootLocalClustering,"lc_364d_slo_con")
 
 #*******************************************CLASIFICATION EXPERIMETS
 
@@ -65,56 +64,72 @@ clasificacion_364d_nrl <- paste0(rootLocalClasificacion,"s_364d_nrl_slo")
 clasificacion_454d_con <- paste0(rootLocalClasificacion,"s_454d_con_slo")
 clasificacion_454d_nrl <- paste0(rootLocalClasificacion,"s_454d_nrl_slo")
 
-#*******************************************DATASETS ROOTS
+#*******************************************DATASETS ROOTS LOCAL
 
-path_454d_raw_umr<- paste0(rootLocalDatasets,"454d_raw_umr/454d_raw_umr")
-path_454d_raw_con<- paste0(rootLocalDatasets,"454d_raw_con/454d_raw_con")
-path_454d_max_umr<- paste0(rootLocalDatasets,"454d_max_umr/454d_max_umr")
-path_454d_max_con<- paste0(rootLocalDatasets,"454d_max_con/454d_max_con")
-path_454d_slo_umr<- paste0(rootLocalDatasets,"454d_slo_umr/454d_slo_umr")
-path_454d_slo_con<- paste0(rootLocalDatasets,"454d_slo_con/454d_slo_con")
+l_path_454d_raw_umr<- paste0(rootLocalDatasets,"454d_raw_umr/454d_raw_umr")
+l_path_454d_raw_con<- paste0(rootLocalDatasets,"454d_raw_con/454d_raw_con")
+l_path_454d_max_umr<- paste0(rootLocalDatasets,"454d_max_umr/454d_max_umr")
+l_path_454d_max_con<- paste0(rootLocalDatasets,"454d_max_con/454d_max_con")
+l_path_454d_slo_umr<- paste0(rootLocalDatasets,"454d_slo_umr/454d_slo_umr")
+l_path_454d_slo_con<- paste0(rootLocalDatasets,"454d_slo_con/454d_slo_con")
 
-path_364d_raw_umr<- paste0(rootLocalDatasets,"364d_raw_umr/364d_raw_umr")
-path_364d_raw_con<- paste0(rootLocalDatasets,"364d_raw_con/364d_raw_con")
-path_364d_raw_umr<- paste0(rootLocalDatasets,"364d_max_umr/364d_max_umr")
-path_364d_raw_con<- paste0(rootLocalDatasets,"364d_max_con/364d_max_con")
-path_364d_slo_umr<- paste0(rootLocalDatasets,"364d_slo_umr/364d_slo_umr")
-path_364d_slo_con<- paste0(rootLocalDatasets,"364d_slo_con/364d_slo_con")
+l_path_364d_raw_umr<- paste0(rootLocalDatasets,"364d_raw_umr/364d_raw_umr")
+l_path_364d_raw_con<- paste0(rootLocalDatasets,"364d_raw_con/364d_raw_con")
+l_path_364d_raw_umr<- paste0(rootLocalDatasets,"364d_max_umr/364d_max_umr")
+l_path_364d_raw_con<- paste0(rootLocalDatasets,"364d_max_con/364d_max_con")
+l_path_364d_slo_umr<- paste0(rootLocalDatasets,"364d_slo_umr/364d_slo_umr")
+l_path_364d_slo_con<- paste0(rootLocalDatasets,"364d_slo_con/364d_slo_con")
+
+#*******************************************DATASETS ROOTS LOCAL
+
+r_path_454d_raw_umr<- paste0(rootHDFSdatasets,"454d_raw_umr/454d_raw_umr")
+r_path_454d_raw_con<- paste0(rootHDFSdatasets,"454d_raw_con/454d_raw_con")
+r_path_454d_max_umr<- paste0(rootHDFSdatasets,"454d_max_umr/454d_max_umr")
+r_path_454d_max_con<- paste0(rootHDFSdatasets,"454d_max_con/454d_max_con")
+r_path_454d_slo_umr<- paste0(rootHDFSdatasets,"454d_slo_umr/454d_slo_umr")
+r_path_454d_slo_con<- paste0(rootHDFSdatasets,"454d_slo_con/454d_slo_con")
+
+r_path_364d_raw_umr<- paste0(rootHDFSdatasets,"364d_raw_umr/364d_raw_umr")
+r_path_364d_raw_con<- paste0(rootHDFSdatasets,"364d_raw_con/364d_raw_con")
+r_path_364d_raw_umr<- paste0(rootHDFSdatasets,"364d_max_umr/364d_max_umr")
+r_path_364d_raw_con<- paste0(rootHDFSdatasets,"364d_max_con/364d_max_con")
+r_path_364d_slo_umr<- paste0(rootHDFSdatasets,"364d_slo_umr/364d_slo_umr")
+r_path_364d_slo_con<- paste0(rootHDFSdatasets,"364d_slo_con/364d_slo_con")
 
 
 #******************************************OLD
-database_parquet <- paste0(rootHDFS,"database_parquet/")
-datasets_parquet <- paste0(rootHDFS,"datasets_parquet/")
-joins_parquet <- paste0(rootHDFS,"joins_parquet/")
-path00C <- paste0(database_parquet,"TAB00C")
-path00E <- paste0(database_parquet,"TAB00E")
-path01 <- paste0(database_parquet,"TAB01")
-path02 <- paste0(database_parquet,"TAB02")
-path05A <- paste0(database_parquet,"TAB05A")
-path05B <- paste0(database_parquet,"TAB05B")
-path05C <- paste0(database_parquet,"TAB05C")
-path06 <- paste0(database_parquet,"TAB06")
-path08 <- paste0(database_parquet,"TAB08")
-path15A <- paste0(database_parquet,"TAB15A")
-path15B <- paste0(database_parquet,"TAB15B")
-path15C <- paste0(database_parquet,"TAB15C")
-path16 <- paste0(database_parquet,"TAB16")
-path24 <- paste0(database_parquet,"TAB24")
-
-database_hdd_david <- paste0(rootHDDavid,"database_parquet/")
-datasets_hdd_david <- paste0(rootHDDavid,"datasets_parquet/")
-joins_hdd_david <- paste0(rootHDDavid,"joins_parquet/")
-path00C_hdd_david <- paste0(database_hdd_david,"TAB00C")
-path00E_hdd_david <- paste0(database_hdd_david,"TAB00E")
-path01_hdd_david <- paste0(database_hdd_david,"TAB01")
-path02_hdd_david <- paste0(database_hdd_david,"TAB02")
-#path05A_hdd_david <- paste0(database_hdd_david,"TAB05A")
-#path05B_hdd_david <- paste0(database_hdd_david,"TAB05B")
-#path05C_hdd_david <- paste0(database_hdd_david,"TAB05C")
-path06_hdd_david <- paste0(database_hdd_david,"TAB06")
-path08_hdd_david <- paste0(database_hdd_david,"TAB08")
-path15A_hdd_david <- paste0(database_hdd_david,"TAB15A")
-path15B_hdd_david <- paste0(database_hdd_david,"TAB15B")
-path15C_hdd_david <- paste0(database_hdd_david,"TAB15C")
-path16_hdd_david <- paste0(database_hdd_david,"TAB16")
-path24_hdd_david <- paste0(database_hdd_david,"TAB24")
+# database_parquet <- paste0(rootHDFS,"database_parquet/")
+# datasets_parquet <- paste0(rootHDFS,"datasets_parquet/")
+# joins_parquet <- paste0(rootHDFS,"joins_parquet/")
+# path00C <- paste0(database_parquet,"TAB00C")
+# path00E <- paste0(database_parquet,"TAB00E")
+# path01 <- paste0(database_parquet,"TAB01")
+# path02 <- paste0(database_parquet,"TAB02")
+# path05A <- paste0(database_parquet,"TAB05A")
+# path05B <- paste0(database_parquet,"TAB05B")
+# path05C <- paste0(database_parquet,"TAB05C")
+# path06 <- paste0(database_parquet,"TAB06")
+# path08 <- paste0(database_parquet,"TAB08")
+# path15A <- paste0(database_parquet,"TAB15A")
+# path15B <- paste0(database_parquet,"TAB15B")
+# path15C <- paste0(database_parquet,"TAB15C")
+# path16 <- paste0(database_parquet,"TAB16")
+# path24 <- paste0(database_parquet,"TAB24")
+# 
+# database_hdd_david <- paste0(rootHDDavid,"database_parquet/")
+# datasets_hdd_david <- paste0(rootHDDavid,"datasets_parquet/")
+# joins_hdd_david <- paste0(rootHDDavid,"joins_parquet/")
+# path00C_hdd_david <- paste0(database_hdd_david,"TAB00C")
+# path00E_hdd_david <- paste0(database_hdd_david,"TAB00E")
+# path01_hdd_david <- paste0(database_hdd_david,"TAB01")
+# path02_hdd_david <- paste0(database_hdd_david,"TAB02")
+# #path05A_hdd_david <- paste0(database_hdd_david,"TAB05A")
+# #path05B_hdd_david <- paste0(database_hdd_david,"TAB05B")
+# #path05C_hdd_david <- paste0(database_hdd_david,"TAB05C")
+# path06_hdd_david <- paste0(database_hdd_david,"TAB06")
+# path08_hdd_david <- paste0(database_hdd_david,"TAB08")
+# path15A_hdd_david <- paste0(database_hdd_david,"TAB15A")
+# path15B_hdd_david <- paste0(database_hdd_david,"TAB15B")
+# path15C_hdd_david <- paste0(database_hdd_david,"TAB15C")
+# path16_hdd_david <- paste0(database_hdd_david,"TAB16")
+# path24_hdd_david <- paste0(database_hdd_david,"TAB24")
--- api/graficas_dataset_t123_d.R
+++ api/graficas_dataset_t123_d.R
@@ -255,9 +255,153 @@ fromStringToNumberDF<-function(df){
 }
 
 
+loadClusteringModels<- function(experimentPath){
+  
+  filePaths <- dir(experimentPath,full.names = T)
+  
+  #print(filePaths)
+  
+  nmodels<- length(filePaths)/3
+  
+  models<- list()
+  
+  for(i in 1:nmodels){
+    
+    models[[i]]<-loadClusteringModelByIndex(i,filePaths)
+    # print(paste0("model ",i," loaded"))
+  }
+  
+  return(models)
+  
+}
 
 
 
+
+
+
+
+loadClusteringModelByIndex <-function(clusteringModelIndex,modelPathsList){
+  
+  nmodels<- length(modelPathsList)/3
+  
+  if(clusteringModelIndex>nmodels){
+    return("Error")
+  }
+  
+  else{
+  
+    
+      
+    cenFileIndex <- clusteringModelIndex
+    cluFileIndex <- nmodels + cenFileIndex
+    stdFileIndex <- nmodels*2 + cenFileIndex
+    
+    # print(paste0(cenFileIndex,"-",cluFileIndex,"-",stdFileIndex))
+    
+    cenPath <- modelPathsList[[cenFileIndex]]
+    cluPath <- modelPathsList[[cluFileIndex]]
+    stdPath <- modelPathsList[[stdFileIndex]]
+    
+    #print(paste0(cenPath,"-",cluPath,"-",stdPath))
+    
+    cen<-read.csv(cenPath,sep=";",header=F)
+    #clu<-as.data.frame(read.parquet(cluPath))
+    clu<-read.parquet(cluPath)
+    std<-read.csv(stdPath,header=T,sep=";")
+    
+    return(list(cen=cen,clu=clu,std=std))
+    
+  }
+}  
+
+getClusteringModelNumber<-function (rootPath){
+  
+  ln <- strsplit(rootPath,"_",fixed=T)[[1]]
+  
+  r <- ln[length(ln)-1]
+  
+  return(r)
+  
+}
+
+
+
+
+getClusteringExperimentHeatMaps<-function(models){
+  
+  hmps <- list(getClusteringHeatMaps(models[[1]][[3]]))
+  
+  for(i in 2:length(models)){
+    
+    model<-models[[i]]
+    
+    hmps[[i]]<- getClusteringHeatMaps(model[[3]])
+    
+    
+  }
+  
+  return(hmps)
+  
+}
+
+
+getClusteringHeatMaps <- function(cluStudy){
+  
+  blues <- brewer.pal(9, "Blues")
+  
+  
+  el<-c()
+  for (e in cluStudy[,1]){
+    print(e)
+    el<-c(el,paste0("cluster #",e))
+  }
+  
+  cluStudy[,1]<-el
+  
+  # print(cluStudy[,1])
+  
+  names(cluStudy)[2]<-"T"
+  names(cluStudy)[3]<-"%T"
+  names(cluStudy)[4]<-"F"
+  names(cluStudy)[5]<-"L"
+  names(cluStudy)[6]<-"%LC"
+  names(cluStudy)[7]<-"%FC"
+  names(cluStudy)[8]<-"%LT"
+  names(cluStudy)[9]<-"%FT"
+  
+  melted_1 <- melt(cluStudy[,c(1,4,5,2)],"cluster")
+  melted_2 <- melt(cluStudy[,c(1,8,9,3)],"cluster")
+  melted_3 <- melt(cluStudy[,c(1,6,7)],"cluster")
+  
+  rl<-list(getHeatMapGraph(melted_1,cluStudy$cluster,blues),
+           getHeatMapGraph(melted_2,cluStudy$cluster,blues),
+           getHeatMapGraph(melted_3,cluStudy$cluster,blues))
+  
+  return(rl)
+  
+}
+
+getHeatMapGraph <- function(melted,labels,pallete){
+  
+  # print(melted$cluster)
+
+    grp<- ggplot(melted, aes(x=variable, y=cluster)) +
+    geom_tile(aes(fill = value),colour="black") + 
+    geom_text(aes(label = value),size=2) +
+    scale_fill_gradientn(colours =pallete[1:5] )+
+    scale_y_discrete(limits= rev(labels))+
+    theme(axis.title.x=element_blank(),
+          axis.ticks.x =element_blank(),
+          axis.title.y=element_blank(),
+          axis.ticks.y =element_blank(),
+          panel.background = element_blank(),
+          text = element_text(size=7),
+          legend.position="none")
+  
+  return(grp)
+  
+}
 
 
 
